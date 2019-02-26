55fcab1de546300c7f45a6d3bb504965d77dea0d
yaonan <yaonan.zhang@edu.uwaterloo.ca>
2019-02-26 14:47:45 -0500

    step1, create,open and close

---
+++ watdfs_client.c
@@ -0,0 +1,336 @@
+//
+// Starter code for CS 454
+// You SHOULD change this file
+//
+//
+
+#include "watdfs_client.h"
+
+#include "rpc.h"
+#include "fuse.h"
+
+#ifdef PRINT_ERR
+#include <cstdio>
+#endif
+
+// You may want to include iostream or cstdio.h if you print to standard error.
+
+// SETUP AND TEARDOWN
+void *watdfs_cli_init(struct fuse_conn_info *conn, const char *path_to_cache,
+		time_t cache_interval) {
+	// You should set up the RPC library here, by calling rpcClientInit.
+	int rirt = rpcClientInit();
+	// You should check the return code of the rpcClientInit call, as it may fail,
+	// for example, if the incorrect port was exported. If there was an error,
+	// it may be useful to print to stderr or stdout during debugging.
+	// Important: Make sure you turn off logging prior to submission!
+	// One useful technique is to use pre-processor flags like:
+	// # ifdef PRINT_ERR
+	// fprintf(stderr, "Failed to initialize RPC Client\n");
+	// Or if you prefer c++:
+	// std::cerr << "Failed to initialize RPC Client";
+	// #endif
+	if (rirt < 0)
+	{
+#ifdef PRINT_ERR
+		printf("[%d]\n",rirt);
+#endif
+	}
+
+
+	// You can also initialize any global state that you want to have in this
+	// method, and return it. The value that you return here will be passed
+	// as userdata in other functions.
+
+	// path_to_cache and cache_interval are not necessary for Assignment 2, but should
+	// be used in Assignment 3.
+	return NULL;
+}
+
+void watdfs_cli_destroy(void *userdata) {
+	// You should clean up your userdata state here.
+	// You should also tear down the RPC library by calling rpcClientDestroy.
+	rpcClientDestroy();
+}
+
+// GET FILE ATTRIBUTES
+int watdfs_cli_getattr(void *userdata, const char *path, struct stat *statbuf) {
+
+	// SET UP THE RPC CALL
+
+	// getattr has 3 arguments.
+	int num_args = 3;
+
+	// Allocate space for the output arguments.
+	void **args = (void**) malloc(3 * sizeof(void*));
+
+	// Allocate the space for arg types, and one extra space for the null
+	// array element.
+	int arg_types[num_args + 1];
+
+	// The path has string length (strlen) + 1 (for the null character).
+	int pathlen = strlen(path) + 1;
+
+	// Fill in the arguments
+	// The first argument is the path, it is an input only argument, and a char
+	// array. The length of the array is the length of the path.
+	arg_types[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | pathlen;
+	// For arrays the argument is the array pointer, not a pointer to a pointer.
+	args[0] = (void*)path;
+
+	// The second argument is the stat structure. This argument is an output
+	// only argument, and we treat it as a char array. The length of the array
+	// is the size of the stat structure, which we can determine with sizeof.
+	arg_types[1] = (1 << ARG_OUTPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | sizeof(struct stat); // statbuf
+	args[1] = (void*)statbuf;
+
+	// The third argument is the return code, an output only argument, which is
+	// an integer. You should fill in this argument type here:
+	arg_types[2] = (1 << ARG_OUTPUT) | (ARG_INT << 16);
+
+	// The return code is not an array, so we need to hand args[2] an int*.
+	// The int* could be the address of an integer located on the stack, or use
+	// a heap allocated integer, in which case it should be freed.
+	// You should fill in the argument here:
+	int retcode;
+	args[2] = &retcode;
+
+	// Finally, the last position of the arg types is 0. There is no corresponding
+	// arg.
+	arg_types[3] = 0;
+
+	// MAKE THE RPC CALL
+	int rpc_ret = rpcCall((char *)"getattr", arg_types, args);
+
+	// HANDLE THE RETURN
+
+	// The integer value watdfs_cli_getattr will return.
+	int fxn_ret = 0;
+	if (rpc_ret < 0) {
+		// Something went wrong with the rpcCall, return a sensible return value.
+		// In this case lets return, -EINVAL
+		fxn_ret = -EINVAL;
+	} else {
+		// Our RPC call succeeded. However, it's possible that the return code
+		// from the server is not 0, that is it may be -errno. Therefore, we should
+		// set our function return value to the retcode from the server.
+		// You should set the function return variable to the return code from the
+		// server here:
+		if (retcode < 0) {
+			fxn_ret = retcode;
+		}
+	}
+
+	if (fxn_ret < 0) {
+		// Important: if the return code of watdfs_cli_getattr is negative (an
+		// error), then we need to make sure that the stat structure is filled with
+		// 0s. Otherwise, FUSE will be confused by the contradicting return values.
+		memset(statbuf, 0, sizeof(struct stat));
+	}
+
+	// Clean up the memory we have allocated.
+	free(args);
+
+	// Finally return the value we got from the server.
+	return fxn_ret;
+}
+
+int watdfs_cli_fgetattr(void *userdata, const char *path, struct stat *statbuf,
+		struct fuse_file_info *fi) {
+	
+	int num_args = 4;
+	void **args = (void**) malloc(4 * sizeof(void*));
+	int arg_types[num_args + 1];
+	int pathlen = strlen(path) + 1;
+
+	arg_types[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | pathlen;
+	args[0] = (void*)path;
+
+	arg_types[1] = (1 << ARG_OUTPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | sizeof(struct stat); // statbuf
+	args[1] = (void*)statbuf;
+
+	arg_types[2] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | sizeof(struct fuse_file_info);  //fi
+	args[2] = (void*)fi;
+	
+	arg_types[3] = (1 << ARG_OUTPUT) | (ARG_INT << 16);
+	int retcode;
+	args[3] = &retcode;
+
+	arg_types[4] = 0;
+
+	int rpc_ret = rpcCall((char *)"fgetattr", arg_types, args);
+	
+	int fxn_ret = 0;
+	if (rpc_ret < 0) 
+	{
+		fxn_ret = -EINVAL;
+	} 
+	else 
+	{
+		if (retcode < 0) 
+		{
+			fxn_ret = retcode;
+		}
+	}
+	
+	free(args);
+	return fxn_ret;
+}
+
+// CREATE, OPEN AND CLOSE
+int watdfs_cli_mknod(void *userdata, const char *path, mode_t mode, dev_t dev) {
+	// Called to create a file.
+	int num_args = 4;
+	void **args = (void**) malloc( 4 * sizeof(void*));
+	int arg_types[num_args + 1];
+	int pathlen = strlen(path) + 1;
+
+	arg_types[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | pathlen;  //path
+	args[0] = (void*)path;
+
+	arg_types[1] = (1 << ARG_INPUT) | (ARG_INT << 16);      //mod
+	args[1] = &mode;
+
+	arg_types[2] = (1 << ARG_INPUT) | (ARG_LONG << 16);  //dev
+	args[2] = &dev;
+
+	arg_types[3] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	int retcode;
+	args[3] = &retcode;
+
+	arg_types[4] = 0;
+
+	int rpc_ret = rpcCall((char *)"mknod", arg_types, args);
+
+	int fxn_ret = 0;
+	if (rpc_ret < 0) 
+	{
+		fxn_ret = -EINVAL;
+	} 
+	else 
+	{
+		if (retcode < 0) 
+		{
+			fxn_ret = retcode;
+		}
+	}
+
+	free(args);
+	return fxn_ret;
+}
+
+int watdfs_cli_open(void *userdata, const char *path, struct fuse_file_info *fi) {
+	// Called during open.
+	// You should fill in fi->fh.
+	
+	int num_args = 3;
+	void **args = (void**) malloc(3 * sizeof(void*));
+	int arg_types[num_args + 1];
+	int pathlen = strlen(path) + 1;
+      
+	arg_types[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | pathlen;  //path
+	args[0] = (void*) path;
+
+    arg_types[1] = (1 << ARG_INPUT) | (1 << ARG_OUTPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | sizeof(fuse_file_info);  //fi
+    args[1] = (void*)fi;
+
+	arg_types[2] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	int retcode;
+	args[2] = &retcode;
+
+	
+	arg_types[3] = 0;
+	
+	int rpc_ret = rpcCall((char *)"open", arg_types, args);
+
+	int fxn_ret = 0;
+	if (rpc_ret < 0) 
+	{
+		fxn_ret = -EINVAL;
+	} 
+	else 
+	{
+		if (retcode < 0) 
+		{
+			fxn_ret = retcode;
+		}
+	}
+
+	free(args);
+	return fxn_ret;
+}
+
+int watdfs_cli_release(void *userdata, const char *path,
+		struct fuse_file_info *fi) {
+	// Called during close, but possibly asynchronously.
+	int num_args = 3;
+	void **args = (void**) malloc(3 * sizeof(void*));
+	int arg_types[num_args + 1];
+	int pathlen = strlen(path) + 1;
+      
+	arg_types[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | pathlen;  //path
+	args[0] = (void*) path;
+
+    arg_types[1] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | sizeof(fuse_file_info);  //fi
+    args[1] = (void*)fi;
+
+	arg_types[2] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	int retcode;
+	args[2] = &retcode;
+
+	
+	arg_types[3] = 0;
+	
+	int rpc_ret = rpcCall((char *)"release", arg_types, args);
+
+	int fxn_ret = 0;
+	if (rpc_ret < 0) 
+	{
+		fxn_ret = -EINVAL;
+	} 
+	else 
+	{
+		if (retcode < 0) 
+		{
+			fxn_ret = retcode;
+		}
+	}
+
+	free(args);
+	return fxn_ret;
+}
+
+// READ AND WRITE DATA
+int watdfs_cli_read(void *userdata, const char *path, char *buf, size_t size,
+		off_t offset, struct fuse_file_info *fi) {
+	// Read size amount of data at offset of file into buf.
+
+	// Remember that size may be greater then the maximum array size of the RPC
+	// library.
+	return -ENOSYS;
+}
+int watdfs_cli_write(void *userdata, const char *path, const char *buf,
+		size_t size, off_t offset, struct fuse_file_info *fi) {
+	// Write size amount of data at offset of file from buf.
+
+	// Remember that size may be greater then the maximum array size of the RPC
+	// library.
+	return -ENOSYS;
+}
+int watdfs_cli_truncate(void *userdata, const char *path, off_t newsize) {
+	// Change the file size to newsize.
+	return -ENOSYS;
+}
+
+int watdfs_cli_fsync(void *userdata, const char *path,
+		struct fuse_file_info *fi) {
+	// Force a flush of file data.
+	return -ENOSYS;
+}
+
+// CHANGE METADATA
+int watdfs_cli_utimens(void *userdata, const char *path,
+		const struct timespec ts[2]) {
+	// Change file access and modification times.
+	return -ENOSYS;
+}
---
+++ watdfs_server.c
@@ -0,0 +1,311 @@
+//
+// Starter code for CS 454
+// You SHOULD change this file
+//
+//
+
+#include "rpc.h"
+#include "fuse.h"
+
+// You may need to change your includes depending on whether you use C or C++.
+
+// Needed for stat.
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Needed for errors.
+#include <errno.h>
+
+// Needed for string operations.
+#include <cstring>
+
+// Need malloc and free.
+#include <cstdlib>
+
+// You may want to include iostream or cstdio.h if you print to standard error.
+#ifdef PRINT_ERR
+#include <cstdio> 
+#endif
+
+// Global state server_persist_dir.
+char *server_persist_dir = NULL;
+
+// We need to operate on the path relative to the the server_persist_dir.
+// This function returns a path that appends the given short path to the
+// server_persist_dir. The character array is allocated on the heap, therefore
+// it should be freed after use.
+char* get_full_path(char *short_path) {
+  int short_path_len = strlen(short_path);
+  int dir_len = strlen(server_persist_dir);
+  int full_len = dir_len + short_path_len + 1;
+
+  char *full_path = (char*)malloc(full_len);
+
+  // First fill in the directory.
+  strcpy(full_path, server_persist_dir);
+  // Then append the path.
+  strcat(full_path, short_path);
+
+  return full_path;
+}
+
+// The server implementation of getattr.
+int watdfs_getattr(int *argTypes, void **args) {
+  // Get the arguments.
+  // The first argument is the path relative to the mountpoint.
+  char *short_path = (char*)args[0];
+  // The second argument is the stat structure, which should be filled in
+  // by this function.
+  struct stat *statbuf = (struct stat*)args[1];
+  // The third argument is the return code, which will be 0, or -errno.
+  int *ret = (int*)args[2];
+
+  // Get the local file name, so we call our helper function which appends
+  // the server_persist_dir to the given path.
+  char *full_path = get_full_path(short_path);
+
+  // Initially we set set the return code to be 0.
+  *ret = 0;
+
+  // Make the stat system call, which is the corresponding system call needed
+  // to support getattr. You should make the stat system call here:
+
+  // Let sys_ret be the return code from the stat system call.
+  int sys_ret = stat(full_path, statbuf);
+
+  // You should use the statbuf as an argument to the stat system call, but it
+  // is currently unused.
+  //(void)statbuf;
+
+  if (sys_ret < 0) {
+    // If there is an error on the system call, then the return code should
+    // be -errno.
+    *ret = -errno;
+  }
+
+  // Clean up the full path, it was allocated on the heap.
+  free(full_path);
+
+  // The RPC call should always succeed, so return 0.
+  return 0;
+}
+
+int watdfs_fgetattr(int *argTypes, void **args) {
+	
+	struct stat *statbuf = (struct stat*)args[1];
+
+	struct fuse_file_info *fi = (struct fuse_file_info*)args[2];
+	int *ret = (int*)args[3];
+  
+	*ret = 0;
+
+	int sys_ret = fstat(fi->fh, statbuf);
+	
+	if(sys_ret < 0) {
+		*ret = -errno;
+	}
+
+	return 0;
+
+}
+
+int watdfs_mknod(int *argTypes, void **args) {
+  
+	char *short_path = (char*)args[0];
+	int mod = *(int*)args[1];
+	long dev = *(long*)args[2];
+	int *ret = (int*)args[3];
+  
+	char *full_path = get_full_path(short_path);
+	*ret = 0;
+
+	int sys_ret = mknod(full_path, mod, dev);
+
+	if(sys_ret < 0) {
+		*ret = -errno;
+	}
+
+	free(full_path);
+	return 0;
+}
+
+int watdfs_open(int *argTypes, void **args) {
+	
+	char *short_path = (char*)args[0];
+	struct fuse_file_info *fi = (struct fuse_file_info*)args[1];
+	int *ret = (int*)args[2];
+
+	char *full_path = get_full_path(short_path);
+	*ret = 0;
+
+	int sys_ret = open(full_path, fi->flags);
+
+	if(sys_ret < 0) 
+		*ret = -errno;
+	else
+		fi->fh = sys_ret;
+
+	free(full_path);
+	return 0;
+}
+
+int watdfs_release(int *argTypes, void **args) {
+	
+	char *short_path = (char*)args[0];
+	struct fuse_file_info *fi = (struct fuse_file_info*)args[1];
+	int *ret = (int*)args[2];
+	
+	char *full_path = get_full_path(short_path);
+	*ret = 0;
+
+	int sys_ret = close(fi->fh);
+	
+	if(sys_ret < 0) 
+		*ret = -errno;
+	else
+		fi->fh = sys_ret;
+
+	free(full_path);
+	return 0;
+}
+
+// The main function of the server.
+int main(int argc, char *argv[]) {
+  // argv[1] should contain the directory where you should store data on the
+  // server. If it is not present it is an error, that we cannot recover from.
+  if (argc != 2) {
+    // In general you shouldn't print to stderr or stdout, but it may be
+    // helpful here for debugging. Important: Make sure you turn off logging
+    // prior to submission!
+    // See watdfs_client.c for more details
+    // # ifdef PRINT_ERR
+    // fprintf(stderr, "Usage: %s server_persist_dir\n", argv[0]);
+    // Or if you prefer c++:
+    // std::cerr << "Usaage:" << argv[0] << " server_persist_dir";
+    // #endif
+#ifdef PRINT_ERR
+	printf("bad argc\n");
+#endif
+    return -1;
+  }
+  // Store the directory in a global variable.
+  server_persist_dir = argv[1];
+
+  // Initialize the rpc library by calling rpcServerInit. You should call
+  // rpcServerInit here:
+  int ret = rpcServerInit();
+  if (ret < 0)
+  {
+#ifdef PRINT_ERR
+	printf("prcSI ret:%d\n",ret);
+#endif
+	return ret;
+  }
+
+  // If there is an error with rpcServerInit, it maybe useful to have
+  // debug-printing here, and then you should return.
+
+  // Register your functions with the RPC library.
+  // getattr
+  {
+      // There are 3 args for the function (see watdfs_client.c for more detail).
+      int argTypes[4];
+      // First is the path.
+      argTypes[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;
+      // Note for arrays we can set the length to be anything  > 1.
+
+      // The second argument is the statbuf.
+      argTypes[1] = (1 << ARG_OUTPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;
+      // The third argument is the retcode.
+      argTypes[2] = (1 << ARG_OUTPUT) | (ARG_INT << 16);
+      // Finally we fill in the null terminator.
+      argTypes[3] = 0;
+
+      // We need to register the function with the types and the name.
+      ret = rpcRegister((char*)"getattr", argTypes, watdfs_getattr);
+      if (ret < 0) {
+        // It may be useful to have debug-printing here.
+#ifdef PRINT_ERR
+		printf("getattr rpc Reg bad!\n");
+#endif
+        return ret;
+      }
+  }
+
+  // fgetattr
+  {
+	  int argTypes[5];
+
+      argTypes[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //path
+      argTypes[1] = (1 << ARG_OUTPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1; //statbuf
+      argTypes[2] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //fi
+      argTypes[3] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	  argTypes[4] = 0;
+
+      ret = rpcRegister((char*)"fgetattr", argTypes, watdfs_fgetattr);
+      if (ret < 0) {
+#ifdef PRINT_ERR
+		printf("fgetattr rpc Reg bad!\n");
+#endif
+        return ret;
+      }
+  }
+
+  //mknod
+  {
+	  int argTypes[5];
+
+      argTypes[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //path
+      argTypes[1] = (1 << ARG_INPUT) | (ARG_INT << 16);      //mod
+      argTypes[2] = (1 << ARG_INPUT) | (ARG_LONG << 16);  //dev
+      argTypes[3] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	  argTypes[4] = 0;
+
+      ret = rpcRegister((char*)"mknod", argTypes, watdfs_mknod);
+      if (ret < 0) {
+#ifdef PRINT_ERR
+		printf("mknod rpc Reg bad!\n");
+#endif
+        return ret;
+      }
+  }
+
+  //open
+  {
+	  int argTypes[4];
+      argTypes[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //path
+      argTypes[1] = (1 << ARG_INPUT) | (1 << ARG_OUTPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //fi
+      argTypes[2] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	  argTypes[3] = 0;
+      
+	  ret = rpcRegister((char*)"open", argTypes, watdfs_open);
+      if (ret < 0)
+        return ret;
+  }
+
+  //release
+  {
+	  int argTypes[4];
+      argTypes[0] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //path
+      argTypes[1] = (1 << ARG_INPUT) | (1 << ARG_ARRAY) | (ARG_CHAR << 16) | 1;  //fi
+      argTypes[2] = (1 << ARG_OUTPUT) | (ARG_INT << 16); //retcode
+	  argTypes[3] = 0;
+      
+	  ret = rpcRegister((char*)"release", argTypes, watdfs_release);
+      if (ret < 0)
+        return ret;
+  }
+
+  // Hand over control to the RPC library by calling rpcExecute. You should call
+  // rpcExecute here:
+
+  ret = rpcExecute();
+  // rpcExecute could fail so you may want to have debug-printing here, and then
+  // you should return.
+#ifdef PRINT_ERR
+  if(ret < 0)
+	  printf("rpc exec fail:%d\n", ret);
+#endif
+  return ret;
+}
